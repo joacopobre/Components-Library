b953e2b56335f3a9e3bd93eb5f4b0e9a63aca63e
joaquinrodes <j.rodes@edu.salford.ac.uk>
2025-08-19 15:07:57 +0100

    Logic and style for Testimonials component

--- src/components/Testimonials/Testimonial.jsx
+++ src/components/Testimonials/Testimonial.jsx
@@ -1,11 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faTowerObservation } from '@fortawesome/free-solid-svg-icons'
-
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
 
 export default function Testimonial({icon=null,name,job,children}){
     const className = icon ? "testimonial with-icon" : "testimonial no-icon"
+    
     return(
         <section className={className}>
+            
             {icon && (
                 <img 
                     src={icon}
@@ -13,19 +15,20 @@ export default function Testimonial({icon=null,name,job,children}){
                     className="profile"
                 />
             )}
-            <div className="content">
-                {icon ? (<span className="quote-mark">"</span>):
-                (<h1>
-                    <FontAwesomeIcon icon={faTowerObservation} className="logo"/>
-                    Work<span className="cation">cation</span>
-                    </h1>)}
-                <p>"{children}"</p>
-            </div>
-            <div className="footer">
-                <p className="name">{name}</p>
-                <p className="role">{job}</p>
-            </div>
-
+            <section className="text-content">
+                <div className="content">
+                    {icon ? (<span><FontAwesomeIcon icon={faQuoteLeft}className="quote-mark" /></span>):
+                    (<h1>
+                        <FontAwesomeIcon icon={faTowerObservation} className="logo"/>
+                        Work<span className="cation">cation</span>
+                        </h1>)}
+                    {icon? <p>{children}</p>:<p>"{children}"</p>}
+                </div>
+                <div className="footer">
+                    <p className="name">{name}</p>
+                    <p className="role">{job}</p>
+                </div>
+            </section>
         </section>
     )
     
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,13 +1,21 @@
 import Testimonial from "./Testimonial";
+import icon from "../../sources/profile.jpg"
+
 
 export default function Testimonials(){
     return(
         <>  
+            <Testimonial name="May Andersons" job="Workcation, CTO" icon={icon}> 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                 Sed urna nulla vitae laoreet augue.
+                 Amet feugiat est integer dolor auctor adipiscing nunc urna, sit. 
+            </Testimonial>
             <Testimonial name="May Andersons" job="Workcation, CTO"> 
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                  Sed urna nulla vitae laoreet augue.
                  Amet feugiat est integer dolor auctor adipiscing nunc urna, sit. 
             </Testimonial>
+
         </>
     )
 }
